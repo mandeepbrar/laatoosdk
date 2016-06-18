3dc7df1bdd0cdd069f7072a9faaa7ab2c9f5831d
Mandeep <mandeep@virsainfomedia.com>
2016-06-19 00:14:54 +0530

    changes for image mgmt

--- components/storage.go
+++ components/storage.go
@@ -9,6 +9,7 @@ type StorageComponent interface {
 	Open(ctx core.RequestContext, fileName string) (io.ReadCloser, error)
 	SaveFile(ctx core.RequestContext, inpStr io.ReadCloser, fileName string) (string, error)
 	GetFullPath(ctx core.RequestContext, fileName string) string
+	ServeFile(ctx core.RequestContext, fileName string) error
 	CreateFile(ctx core.RequestContext, fileName string) (io.WriteCloser, error)
 	Exists(ctx core.RequestContext, fileName string) bool
 }
