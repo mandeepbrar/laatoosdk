d57aa7e484054878bed2aad7696d1ad22ad1c437
Mandeep Singh Brar <mandeep@virsainfomedia.com>
2016-08-04 22:48:49 +0530

    changes for background processing. Task services do not serve till task manager starts

--- components/taskqueue.go
+++ components/taskqueue.go
@@ -4,6 +4,16 @@ import (
 	"laatoo/sdk/core"
 )
 
+type Task struct {
+	Queue string
+	Data  []byte
+	Token string
+}
+
 type TaskQueue interface {
-	PushTask(ctx core.RequestContext, queue string, task interface{}) error
+	PushTask(ctx core.RequestContext, queue string, task *Task) error
+}
+
+type TaskServer interface {
+	SubsribeQueue(ctx core.ServerContext, queue string) error
 }
