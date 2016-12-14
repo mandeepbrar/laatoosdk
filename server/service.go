2d0e09048b6735ad3f45260af090d3cbfe0e2852
Mandeep Singh Brar <mandeep@virsainfomedia.com>
2016-12-14 11:45:10 +0530

    fixes for headers support

--- server/service.go
+++ server/service.go
@@ -1,11 +1,13 @@
 package server
 
 import (
+	"laatoo/sdk/config"
 	"laatoo/sdk/core"
 )
 
 type Service interface {
 	core.ServerElement
 	Service() core.Service
+	Config() config.Config
 	Invoke(ctx core.RequestContext) error
 }
