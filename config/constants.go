3c32f4e8db43bc3a020cdd8f15405f784ccddd88
Mandeep Singh Brar <brar.ms@gmail.com>
2017-06-21 14:58:12 +0530

    reduce sdk config

--- config/constants.go
+++ config/constants.go
@@ -1,12 +1,15 @@
 package config
 
 const (
+	CONF_CACHE_ENC = "encoding"
+)
+
+const (
 	REALM              = "Realm"
 	DEFAULT_ROLE       = "Role"
 	DEFAULT_ADMIN      = "Admin"
 	DEFAULT_USER       = "User"
 	DEFAULT_AUTHHEADER = "X-Auth-Token"
-	DEFAULT_JWTSECRET  = "SeCrEt"
 	JWTSECRET          = "__jwtsecret"
 	AUTHHEADER         = "__authheader"
 	ADMINROLE          = "__adminrole"
