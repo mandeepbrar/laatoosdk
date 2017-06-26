e194e2f32484c411f00cfaa9bb19ade81fbca153
Mandeep Singh Brar <brar.ms@gmail.com>
2017-06-27 00:06:11 +0530

    changes for laatoo 3

--- core/factoryadapter.go
+++ core/factoryadapter.go
@@ -5,26 +5,28 @@ import (
 )
 
 //service method for doing various tasks
-func NewFactory(creator ObjectCreator) ServiceFactory {
-	return &factoryImpl{creator: creator}
+func NewFactory(creator ObjectCreator) ObjectCreator {
+	return func() interface{} {
+		return &FactoryImpl{creator: creator}
+	}
 }
 
-type factoryImpl struct {
+type FactoryImpl struct {
 	creator ObjectCreator
 }
 
-func (fac *factoryImpl) Initialize(ctx ServerContext, conf config.Config) error {
+func (fac *FactoryImpl) Initialize(ctx ServerContext, conf config.Config) error {
 	return nil
 }
 
 //Create the services configured for factory.
-func (fac *factoryImpl) CreateService(ctx ServerContext, name string, method string, conf config.Config) (Service, error) {
+func (fac *FactoryImpl) CreateService(ctx ServerContext, name string, method string, conf config.Config) (Service, error) {
 	obj := fac.creator()
 	svc, _ := obj.(Service)
 	return svc, nil
 }
 
 //The services start serving when this method is called
-func (fac *factoryImpl) Start(ctx ServerContext) error {
+func (fac *FactoryImpl) Start(ctx ServerContext) error {
 	return nil
 }
