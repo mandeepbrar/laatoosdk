71ff74301efe706c330102b4ac80b51dc8798e0a
Mandeep Singh Brar <mandeep@virsainfomedia.com>
2017-08-26 23:57:00 +0530

    changes for ui plugins

---
+++ components/moduleplugin.go
@@ -0,0 +1,11 @@
+package components
+
+import (
+	"laatoo/sdk/config"
+	"laatoo/sdk/core"
+)
+
+type ModuleManagerPlugin interface {
+	Load(ctx core.ServerContext, name, dir string, mod core.Module, conf config.Config) error
+	Loaded(ctx core.ServerContext) error
+}
--- server/module.go
+++ server/module.go
@@ -4,4 +4,5 @@ import "laatoo/sdk/core"
 
 type Module interface {
 	core.ServerElement
+	GetObject() core.Module
 }
