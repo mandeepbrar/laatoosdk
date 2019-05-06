e85832504a5e156e0f917a9bc548b4ab6b1e0074
Mandeep Singh Brar <mandeep@virsainfomedia.com>
2019-05-06 23:13:29 +0530

    fixes for hotload

--- server/elements/channel.go
+++ server/elements/channel.go
@@ -10,5 +10,5 @@ type Channel interface {
 	GetServiceName() string
 	Serve(ctx core.ServerContext) error
 	Child(ctx core.ServerContext, name string, channelConfig config.Config) (Channel, error)
-	RemoveChild(ctx core.ServerContext, name string, channelConfig config.Config) error
+	Destruct(ctx core.ServerContext, parentChannel Channel) error
 }
