ff9307263b430cfffcffdac1738ef8a799caa2c1
Mandeep Singh Brar <mandeep@virsainfomedia.com>
2019-08-30 08:12:03 +0530

    testing modulesbase

--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module laatoo/sdk
 go 1.12
 
 require (
+	github.com/imdario/mergo v0.3.7
 	github.com/myesui/uuid v1.0.0 // indirect
 	github.com/pmylund/go-cache v2.1.0+incompatible
 	github.com/twinj/uuid v1.0.0
-	golang.org/x/crypto v0.0.0-20190510104115-cbcb75029529
-	golang.org/x/net v0.0.0-20190509222800-a4d6f7feada5
+	golang.org/x/crypto v0.0.0-20190829043050-9756ffdc2472
+	golang.org/x/net v0.0.0-20190827160401-ba9fcec4b297
+	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
--- server.go
+++ server.go
@@ -1,9 +1,17 @@
 package sdk
 
 import (
-	"laatoo/framework/core/server"
+	_ "laatoo/sdk/server/core"
+	_ "laatoo/sdk/utils"
+	_ "laatoo/sdk/common/config"
+	_ "laatoo/sdk/server/auth"
+	_ "laatoo/sdk/server/elements"
+	_ "laatoo/sdk/server/components"
+	_ "laatoo/sdk/server/components/data"
+	_ "laatoo/sdk/server/components/rules"
+	_ "laatoo/sdk/server/components/search"
+	_ "laatoo/sdk/server/errors"
+	_ "laatoo/sdk/server/log"
+	_ "laatoo/sdk/server/registry"
 )
 
-func Server(config string) error {
-	return server.Main(config)
-}
