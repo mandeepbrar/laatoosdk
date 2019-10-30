63f0d505cca8d4cd6784e9c7f154582c3511640c
Mandeep Singh Brar <mandeep@virsainfomedia.com>
2019-10-30 11:16:36 +0530

    fixes for email verification

--- server/components/communicator.go
+++ server/components/communicator.go
@@ -2,6 +2,15 @@ package components
 
 import "laatoo/sdk/server/core"
 
+type Communication struct {
+	Subject     string
+	Mime        string
+	Attachments []string
+	Recipients  map[string]string
+	Message     []byte
+	Info        interface{}
+}
+
 type Communicator interface {
-	SendCommunication(ctx core.RequestContext, communication map[interface{}]interface{}) error
+	SendCommunication(ctx core.RequestContext, communication *Communication) error
 }
--- server/core/requestcontext.go
+++ server/core/requestcontext.go
@@ -41,7 +41,7 @@ type RequestContext interface {
 	GetObjectsFromCache(bucket string, keys []string, objectType string) map[string]interface{}
 	PushTask(queue string, task interface{}) error
 	InvalidateCache(bucket string, key string) error
-	SendCommunication(communication map[interface{}]interface{}) error
+	SendCommunication(communication interface{}) error
 	IsAdmin() bool
 	CompleteRequest()
 }
