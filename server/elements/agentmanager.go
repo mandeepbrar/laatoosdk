72318e6430250a7c899788b2f73f201f4351762d
Mandeep Singh Brar <mandeep@virsainfomedia.com>
2025-11-29 18:57:54 +0530

    changes for agents

--- server/elements/agentmanager.go
+++ server/elements/agentmanager.go
@@ -9,6 +9,8 @@ type AgentManager interface {
 	core.ServerElement
 	GetAgent(ctx core.ServerContext, alias string) (core.Agent, error)
 	List(ctx core.ServerContext) utils.StringsMap
+	GetModels(ctx core.ServerContext, models []string) (utils.StringMap, error)
+	GetTools(ctx core.ServerContext, tools []string) (utils.StringMap, error)
 }
 
 type AgentEngine interface {
