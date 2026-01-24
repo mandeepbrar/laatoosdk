18df859166e6837d4991f22519ae2c1f6dc33d76
Mandeep Singh Brar <mandeep@virsainfomedia.com>
2026-01-24 22:26:14 +0530

    list methods for mcp

--- server/components/mcp.go
+++ server/components/mcp.go
@@ -11,14 +11,23 @@ type Mcp interface {
 	// Tools
 	AddTool(ctx core.ServerContext, tool interface{}, handler interface{}) error
 	RemoveTools(ctx core.ServerContext, names ...string) error
+	GetTool(ctx core.ServerContext, name string) (interface{}, error)
+	ListTools(ctx core.ServerContext) ([]interface{}, error)
 
 	// Prompts
 	AddPrompt(ctx core.ServerContext, prompt interface{}, handler interface{}) error
 	RemovePrompts(ctx core.ServerContext, names ...string) error
+	GetPrompt(ctx core.ServerContext, name string) (interface{}, error)
+	ListPrompts(ctx core.ServerContext) ([]interface{}, error)
 
 	// Resources
 	AddResource(ctx core.ServerContext, resource interface{}, handler interface{}) error
 	RemoveResources(ctx core.ServerContext, uris ...string) error
+	GetResource(ctx core.ServerContext, uri string) (interface{}, error)
+	ListResources(ctx core.ServerContext) ([]interface{}, error)
+
 	AddResourceTemplate(ctx core.ServerContext, template interface{}, handler interface{}) error
 	RemoveResourceTemplates(ctx core.ServerContext, templates ...string) error
+	GetResourceTemplate(ctx core.ServerContext, template string) (interface{}, error)
+	ListResourceTemplates(ctx core.ServerContext) ([]interface{}, error)
 }
