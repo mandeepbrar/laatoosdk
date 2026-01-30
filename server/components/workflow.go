d235794920cb2b99e8f1dfc6c1c73d948a28c21f
Mandeep Singh Brar <mandeep@virsainfomedia.com>
2026-01-31 00:12:00 +0530

    workflow status fixes

--- server/components/workflow.go
+++ server/components/workflow.go
@@ -7,6 +7,17 @@ import (
 	"laatoo.io/sdk/utils"
 )
 
+type WorkflowStatus string
+
+const (
+	WorkflowStatusRunning    WorkflowStatus = "running"
+	WorkflowStatusCompleted  WorkflowStatus = "completed"
+	WorkflowStatusFailed     WorkflowStatus = "failed"
+	WorkflowStatusCanceled   WorkflowStatus = "canceled"
+	WorkflowStatusTerminated WorkflowStatus = "terminated"
+	WorkflowStatusPending    WorkflowStatus = "pending"
+)
+
 type Workflow interface {
 	Spec(ctx context.Context) interface{}
 	Type() string
@@ -16,10 +27,13 @@ type Workflow interface {
 
 type WorkflowInstance interface {
 	GetId() string
-	InstanceDetails() utils.StringMap
-	GetWorkflow() string
-	GetStatus() utils.StringMap
+	GetExecutionId() string
+	GetWorkflowName() string
+	GetStatus() WorkflowStatus
+	GetVariables() utils.StringMap
 	InitData() utils.StringMap
+	GetError() string
+	GetPendingActivities() []string
 }
 
 type WorkflowActivityType string
@@ -33,6 +47,7 @@ const (
 type WorkflowManager interface {
 	LoadWorkflows(ctx core.ServerContext, dir string, module core.Module) (map[string]Workflow, error)
 	StartWorkflow(ctx core.RequestContext, workflowName string, initVal utils.StringMap, insconf utils.StringMap) (WorkflowInstance, error)
+	GetWorkflowInstance(ctx core.RequestContext, instanceId string) (WorkflowInstance, error)
 	IsWorkflowRegistered(ctx core.ServerContext, name string) bool
 	SendSignal(ctx core.RequestContext, workflowId string, workflowIns string, actId string, signal string, signalVal utils.StringMap) error
 	CompleteActivity(ctx core.RequestContext, workflowId string, workflowIns string, actId string, data utils.StringMap, err error) error
