540a8a2844bf275dbff54612414730ad72d4be3e
Mandeep Singh Brar <mandeep@virsainfomedia.com>
2026-02-01 12:43:10 +0530

    module method

--- server/elements/modulemanager.go
+++ server/elements/modulemanager.go
@@ -10,4 +10,5 @@ type ModuleManager interface {
 	List(ctx core.ServerContext) utils.StringsMap
 	Describe(ctx core.ServerContext, mod string) (utils.StringMap, error)
 	ChangeLogger(ctx core.ServerContext, mod string, logLevel string, logFormat string) error
+	GetModule(ctx core.ServerContext, modName string) (core.Module, error)
 }
