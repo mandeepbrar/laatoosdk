01c2d4bdc867f8cd8dc8d74b9b28dd5cc6148de6
Mandeep Singh Brar <mandeep@virsainfomedia.com>
2026-02-12 09:28:05 +0530

    workflow fix

--- server/elements/workflowmanager.go
+++ server/elements/workflowmanager.go
@@ -5,7 +5,17 @@ import (
 	"laatoo.io/sdk/server/core"
 )
 
+type WorkflowType string
+
+const (
+	WorkflowTypeFunction WorkflowType = "function"
+	WorkflowTypeProcess  WorkflowType = "process"
+	WorkflowTypeDurable  WorkflowType = "durable"
+	WorkflowTypeAgent    WorkflowType = "agent"
+)
+
 type WorkflowManager interface {
 	core.ServerElement
 	components.WorkflowManager
+	RegisterProvider(wfType WorkflowType, mgr components.WorkflowManager)
 }
