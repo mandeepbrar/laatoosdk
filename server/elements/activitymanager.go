ac384600bdeebb9beb58e99cc1f1f42f6ca1df37
Mandeep Singh Brar <mandeep@virsainfomedia.com>
2026-03-01 20:54:53 +0530

    added getactivity method

--- server/elements/activitymanager.go
+++ server/elements/activitymanager.go
@@ -9,4 +9,8 @@ type ActivityManager interface {
 	core.ServerElement
 	RegisterActivity(ctx core.ServerContext, activityName string, executor core.ActivityExecutor) error
 	ExecuteActivity(ctx core.RequestContext, activityName string, params utils.StringMap) (interface{}, error)
+	// GetActivityDefinition returns the ActivityDefinition for a registered activity by name,
+	// or nil if the activity is not found. Used by workflow engines to resolve activity
+	// metadata (e.g. ActivityType) without requiring it to be repeated in the workflow DSL.
+	GetActivityDefinition(ctx core.ServerContext, activityName string) *core.ActivityDefinition
 }
