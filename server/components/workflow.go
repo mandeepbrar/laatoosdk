03496528e6d7079aaf87b332b72bb425f8b2b010
Mandeep Singh Brar <mandeep@virsainfomedia.com>
2026-03-25 17:10:28 +0530

    removed redundant method

--- server/components/workflow.go
+++ server/components/workflow.go
@@ -1,21 +1,9 @@
 package components
 
 import (
-	"strings"
-
 	"laatoo.io/sdk/server/core"
 	"laatoo.io/sdk/utils"
 )
-
-// NormaliseWorkflowInstanceID strips any _sub_* suffix from a workflow instance ID
-// so that signals and resume calls always target the root go-workflows instance.
-// It handles any nesting depth: abc_sub_A_sub_B_sub_C → abc.
-func NormaliseWorkflowInstanceID(id string) string {
-	if idx := strings.Index(id, "_sub_"); idx > 0 {
-		return id[:idx]
-	}
-	return id
-}
 
 type WorkflowStatus string
 
